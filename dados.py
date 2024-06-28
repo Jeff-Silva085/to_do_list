16bccb9079e283b2363dc48ce354b5e66d01a3b8
Jefferson Alves <jeffersonalves0976@gmail.com>
2024-06-28 00:57:30 -0300

    arpimoramento da função 'remover item'

--- dados.py
+++ dados.py
@@ -1,4 +1,3 @@
 tasks = {
-    "1": "Arrumar o quarto",
-    "2": "Limpar o tenis",
+    
 }
--- main.py
+++ main.py
@@ -2,28 +2,37 @@ from dados import tasks
 import os
 
 cont = 0
+numberlist = 0
 
 while cont == 0:
+
     #Mostrar a lista de tarefas
+    print("Sua lista de tarefas:")
+    print("--------------------------------------------")
     for key, value in tasks.items():
             print(f"{key}: {value}")
-
-    escolha = input("[1] Adicionar item\n[2] Remover um item\n[3] Ver a lista de tarefas")
+    print("--------------------------------------------")
+    escolha = input("[1] Adicionar item\n[2] Remover um item\n-> ")
+    os.system("cls")
 
     #-- opções da lista de tarefas --
     #   1 Adicionar nova tarefa
     if escolha == "1":
        novaTarefa = input("Qual tarefa deseja adicionar hoje?: ")
-       tasks["3".format] = novaTarefa 
+       numberlist += 1
+       tasks[f"{numberlist}"] = novaTarefa
+       os.system("cls") 
     
     #   2 Remover uma tarefa
     elif escolha == "2":
-        for key, value in tasks.items():
-            print(f"{key}: {value}")
-
-        apg = input("Digite o numero da tarefa: ")
-        del tasks[apg]
-            
+        if bool(tasks) == False:
+             print("Não há itens na sua lista para remover")
+        else:
+            for key, value in tasks.items():
+                print(f"{key}: {value}")
+                apg = input("Digite o numero da tarefa: ")
+                del tasks[apg]
+                os.system("cls")    
 
         #print(key)
         #contador = key
